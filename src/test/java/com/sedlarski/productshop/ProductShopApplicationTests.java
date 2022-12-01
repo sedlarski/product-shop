4595d988dad105d938aab8192643e9c988fd89aa
sedlarski <sedlarski@yahoo.com>
2022-12-01 15:38:54 -0500

    Initial commit

---
+++ src/test/java/com/sedlarski/productshop/ProductShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedlarski.productshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
